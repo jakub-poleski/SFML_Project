25d0241d4c2660f58eeaaf8887ade61e4d0b49d2
Jakub Poleski <poleski.jakub@outlook.com>
2025-04-30 12:39:53 +0200

    zmiana2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <ctime>
-//tu kiedys powstanie gra
+//tu kiedys powstanie gra //test2
 int losowanie(int zakres)
 {
     int los =rand()%zakres;
